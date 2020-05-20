67714bad40e07253d05c8c2dadaec4183aebd134
Pushkar Sabharwal <pushkar7567@gmail.com>
2020-05-20 08:41:04 -0600

    changing components to class and adding footer

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-      TopHeader()
-  );
+class TopHeader extends React.Component {
+  render () {
+    return (
+      <div id="page-header">
+        <div class="title-header">Path Finding Visualizer</div>
+        <ul class="nav-bar">
+          <li>algorithms:
+              <select>
+                <option>Dijkstra's</option>
+                <option>A* Search</option>
+                <option>Breath First Search</option>
+                <option>Greedy Best First Search</option>
+                <option>Depth First Search</option>
+              </select>
+          </li>
+          <li class="nav-element"><button type="button" class="nav-button">Start</button></li>
+          <li class="nav-element"><button type="button" class="nav-button">Stop</button></li>
+          <li class="nav-element"><button type="button" class="nav-button">Weighted node</button></li>
+          <li class="nav-element"><button type="button" class="nav-button">Tutorial</button></li>
+        </ul>
+      </div>
+    );
+  }  
 }
 
-function TopHeader() {
-  return (
-    <div>
-      <div class="title-header">Path Finding Visualizer</div>
-      <ul class="nav-bar">
-        <li>algorithms:
-            <select>
-              <option>Dijkstra's</option>
-              <option>A* Search</option>
-              <option>Breath First Search</option>
-              <option>Greedy Best First Search</option>
-              <option>Depth First Search</option>
-            </select>
-        </li>
-        <li class="nav-element"><button type="button" class="nav-button">Start</button></li>
-        <li class="nav-element"><button type="button" class="nav-button">Stop</button></li>
-        <li class="nav-element"><button type="button" class="nav-button">Weighted node</button></li>
-        <li class="nav-element"><button type="button" class="nav-button">Tutorial</button></li>
-      </ul>
-    </div>
-  );
+class Footer extends React.Component {
+  render () {
+    return (
+      <div id="page-footer" class="footer">
+        Version 1.0  
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <TopHeader />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
